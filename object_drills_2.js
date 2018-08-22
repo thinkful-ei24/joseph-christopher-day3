52efd2ea2bb4992025ee607257c12f891932bdff
josephhess <readysteadyco@gmail.com>
2018-08-22 11:50:09 -0700

    wip object drills 2

--- object_drills_2.js
+++ object_drills_2.js
@@ -26,4 +26,51 @@ const Obj = {
 
 console.log(Obj.meals[3]);
 
+const people = [
+  { name:  'bob',
+    jobTitle: 'plumber'
+  },
+  { name:  'jim',
+    jobTitle: 'hr',
+    boss: 'bob'
+  },
+  { name: 'carl',
+    jobTitle: 'safety inspector',
+    boss: 'bob'
+  },
+];
+
+for (const person of people){
+  if(!person.boss){
+    console.log(`${person.jobTitle} ${person.name} doesn't report to anybody.`);
+  } else {
+    console.log(`${person.jobTitle} ${person.name} reports to ${person.boss}.`);
+  }
+}
+
+const codedMessage = 'craft block argon meter bells brown croon droop';
+
+const cipher = {
+  a:	2,
+  b:	3,
+  c:	4,
+  d:	5
+};
+
+function decode(word){
+  if(cipher.hasOwnProperty(word[0])){
+    return word[cipher[word[0]] - 1];
+  }
+  return ' ';
+}
+function decodeWords(sentence){ 
+  let decoded = '';
+  let sentenceArr = sentence.split(' ');
+  for(const word of sentenceArr){
+    decoded += decode(word);
+  }
+  return decoded;
+}
+
+console.log(decodeWords(codedMessage));
 
