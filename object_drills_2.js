520c0918556a3f9391b99f3c12b0b3a8a4381ba5
Chris Rudder <cbrudder84@gmail.com>
2018-08-22 16:44:41 -0400

    finised all of object problems

--- object_drills_2.js
+++ object_drills_2.js
@@ -136,8 +136,34 @@ const findOne = function(arr,query){
   return null;
 };
 
-console.log(findOne(HEROES, { id: 1 }));
-console.log(findOne(HEROES, { id: 10 }));
-console.log(findOne(HEROES, { id: 2, name: 'Aquaman' }));
-console.log(findOne(HEROES, { id: 5, squad: 'Justice League' }));
-console.log(findOne(HEROES, { squad: 'Justice League' }));
+
+const Database = {
+    store: {
+      heroes: [
+        { id: 1, name: 'Captain America', squad: 'Avengers' },
+        { id: 2, name: 'Iron Man', squad: 'Avengers' },
+        { id: 3, name: 'Spiderman', squad: 'Avengers' },
+        { id: 4, name: 'Superman', squad: 'Justice League' },
+        { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+        { id: 6, name: 'Aquaman', squad: 'Justice League' },
+        { id: 7, name: 'Hulk', squad: 'Avengers' },
+      ],
+    },
+    findOne: function(query){
+        for (const hero of this.store.heroes){
+            all_attr = true;
+            for(const attr in query){
+            if(!hero.hasOwnProperty(attr) || hero[attr] !== query[attr]){
+                all_attr = false;
+            }
+            }
+            if(all_attr){return hero;}
+        }
+        return null;
+        }    
+    };
+console.log(Database.findOne({ id: 1 }));
+console.log(Database.findOne({ id: 10 }));
+console.log(Database.findOne({ id: 2, name: 'Aquaman' }));
+console.log(Database.findOne({ id: 5, squad: 'Justice League' }));
+console.log(Database.findOne({ squad: 'Justice League' }));
